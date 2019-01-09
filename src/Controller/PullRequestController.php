98e1b0189fdafb82002ee2ea8c7ec010fec30978
Evrard Boulou <evrardb@miway.co.za>
2019-01-09 14:13:05 +0200

    add pull request crud

---
+++ src/Controller/PullRequestController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PullRequest;
+use App\Form\PullRequestType;
+use App\Repository\PullRequestRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Workflow\Registry;
+
+/**
+ * @Route("/pull/request")
+ */
+class PullRequestController extends AbstractController
+{
+    /**
+     * @Route("/", name="pull_request_index", methods={"GET"})
+     */
+    public function index(PullRequestRepository $pullRequestRepository): Response
+    {
+        return $this->render('pull_request/index.html.twig', [
+            'pull_requests' => $pullRequestRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="pull_request_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $pullRequest = new PullRequest();
+        $form = $this->createForm(PullRequestType::class, $pullRequest);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($pullRequest);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('pull_request_index');
+        }
+
+        return $this->render('pull_request/new.html.twig', [
+            'pull_request' => $pullRequest,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="pull_request_show", methods={"GET"})
+     */
+    public function show(PullRequest $pullRequest): Response
+    {
+        return $this->render('pull_request/show.html.twig', [
+            'pull_request' => $pullRequest,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="pull_request_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, PullRequest $pullRequest): Response
+    {
+        $form = $this->createForm(PullRequestType::class, $pullRequest);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('pull_request_index', [
+                'id' => $pullRequest->getId(),
+            ]);
+        }
+
+        return $this->render('pull_request/edit.html.twig', [
+            'pull_request' => $pullRequest,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="pull_request_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, PullRequest $pullRequest): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$pullRequest->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($pullRequest);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('pull_request_index');
+    }
+
+    /**
+     * @Route("/{id}/{transitionName}", name="pull_request_flow")
+     */
+    public function nextFlow(Registry $workflows, PullRequest $pullRequest, string $transitionName): Response
+    {
+        $workflow = $workflows->get($pullRequest, 'pull_request');
+        if ($workflow->can($pullRequest, $transitionName)) {
+            // Update the currentState on the pullRequest
+            try {
+                $workflow->apply($pullRequest, $transitionName);
+
+                $nextStep = array_keys($workflow->getMarking($pullRequest)->getPlaces())[0];
+                $pullRequest->setStatus($nextStep);
+                $this->getDoctrine()->getManager()->merge($pullRequest);
+                $this->getDoctrine()->getManager()->flush();
+                $this->addFlash('success', 'Changes applied successfully');
+
+            } catch (TransitionException $exception) {
+                // ... if the transition is not allowed
+            }
+        }
+
+        return $this->redirectToRoute('pull_request_index');
+    }
+}
